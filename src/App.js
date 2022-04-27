7b2696695117165546ca4524df646e43961c4104
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2022-04-27 20:58:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Docs from './pages/Docs';
+import Navigation from "./components/Navigation";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    < Navigation />
+    <Routes>
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/Docs" element={<Docs/>} />  
+      </Routes>
+    </Router>
+  </>
   );
 }
 
---
+++ src/pages/Docs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Docs = () => {
+  return (
+    <div>
+      <h1>Docs</h1>
+    </div>
+  )
+}
+
+export default Docs;
+
