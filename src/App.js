44f8c6a87d169ea6c8340a81beb7fdc39e65f5b6
geekymca <kumarpankaj12830@gmail.com>
2022-07-31 16:53:48 +0530

    loading

--- src/App.js
+++ src/App.js
@@ -3,25 +3,39 @@ import Home from './pages/Home';
 import Docs from './pages/Docs';
 import Login from './pages/Login';
 import AP from './pages/AP';
-import extra from './pages/Extra';
+// import extra from './pages/Extra';
 import Navigation from "./components/Navigation/Navigation";
-
+import React, { useEffect, useState } from "react";
+import Lottie from 'lottie-react';
+import welcome from './assets/welcome.json';
 
 function App() {
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    console.log("Loading Done");
+    setTimeout(() => {
+      setloading(false);
+    }, 1000);
+    setTimeout(() => {
+      document.getElementById('Loading').style.animation='animate_loading 0.4s'
+    }, 700);
+  }, []);
   return (
     <>
-    <Router>
-    < Navigation />
-    <Routes>
-        <Route path="/" element={<Home/>} /> 
-        <Route path="/Docs" element={<Docs/>} /> 
-        <Route path="/AP" element={<AP/>} />  
-        <Route path="/Extra" element={<extra/>} />  
-        <Route path="/Login" element={<Login/>} />   
-     
-      </Routes>
-    </Router>
-  </>
+      {loading ? <div id="Loading" style={{display:"flex",justifyContent:"center"}}><Lottie animationData={welcome} style={{height:'100vh'}}/></div> :
+        <Router>
+          < Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Docs" element={<Docs />} />
+            <Route path="/AP" element={<AP />} />
+            <Route path="/Extra" element={<extra />} />
+            <Route path="/Login" element={<Login />} />
+
+          </Routes>
+        </Router>
+      }
+    </>
   );
 }
 
